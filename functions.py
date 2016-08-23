ce81f9c0760ea9695d551bbdd103c6ec81e14820
Shimal Harichurn <shimal.h.98@gmail.com>
2016-08-23 21:41:00 +0200

    Added function to 'Draw line'

--- functions.py
+++ functions.py
@@ -4,10 +4,21 @@ import random
 
 def random_starting_position(x, turtle, line_start, line_finish):
     #@Parameters: 
-    # x - denotes position of line along x-axis
-    # turtle - denotes turtle object
-    # line_start - y co-ordinate at start of line
-    # line_finish - y co-ordinate at end of line
+        # x - denotes position of line along x-axis
+        #turtle - denotes turtle object
+        #line_start - y co-ordinate at start of line
+        #line_finish - y co-ordinate at end of line
     turtle.setpos(x, random.randrange(line_start, line_finish))
     
 
+def draw_line(vector, turtle, height, width):
+    #@Parameters: 
+        # vector (x,y) - denotes starting position of line
+        # turtle - denotes turtle object
+        # height - denotes height of line
+    turtle.setpos(vector)
+    turtle.pendown()
+    turtle.width(width)
+    turtle.forward(height)
+    turtle.penup()
+    
