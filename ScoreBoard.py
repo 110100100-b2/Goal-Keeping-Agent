50c1246151fa6302172699408335fd94caee6bfd
KarthikP29 <KarthikP29@users.noreply.github.com>
2016-08-25 11:11:25 +0200

    New ScoreBoard
    I used labels again... But try it, I think it could work... I would like to change the positioning still... Let me know what you guys think

--- ScoreBoard.py
+++ ScoreBoard.py
@@ -1,43 +1,33 @@
-
-
 import tkinter as tk       #Basically importing the python gui function: tkinter
 import time #used for the timer added
-
-score = 0
-
-def counter_label(label):
-  def count():
-    global score
-    
-    score += 1
-    
-    label.config(text=str(score))
-    
-  count()
- 
  
 root = tk.Tk() # Creates a  separate window i.e. the label that should display 
 root.title("Score Board Update") #Title
 
 label = tk.Label(root,  fg="blue", font="Times") # customise defined label
 label.pack() # this sort of adjusts the windows size based on the labels contents
-counter_label(label)
 
-def score(temp): #momentarily informs the user whether a goal or save occured
+def counter_label(temp, score):
   
-  def delete(): #used to clear the label 
+  def delete():
     
-    label.configure(text = "1", bg = "white")
+    label.configure(text = ("The score is", str(score)), bg = "white")
   
+  def destroy():
+    
+    root.destroy()
+    
   if temp == "GOAL!!!":
     
-    label.configure(bg = "red") 
-                     
+    label.configure(bg = "red")
+  
   if temp == "SAVE!!!":
     
     label.configure(bg = "green")
   
-  label.config(text = temp)  
+  label.configure(text = temp)
   label.after(2000, delete)
+  label.after(5000, destroy)
 
+counter_label("GOAL!!!", 2)
 root.mainloop()
